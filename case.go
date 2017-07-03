6948023a0414ab63f74e9bf5f8c801158de11d29
huangxiaoan <huangxiaoan@xunlei.com>
2017-07-03 16:50:40 +0800

    dev

---
+++ case.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+    i :=1;
+    switch(i) {
+		case 1:
+		case 2:
+		case 3:
+		fmt.Print("hello");
+	}
+}
---
+++ case.php
@@ -0,0 +1,13 @@
+<?php
+    $i =1;
+    switch($i) {
+		case 1:
+		case 2:
+		case 3:
+		echo ("hello");
+	}
+
+	echo "\r\n";
+	echo 4<<1;
+	
+	echo "\r\n";
