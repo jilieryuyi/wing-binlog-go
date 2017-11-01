9f306f7f29ab04ffcf9a7fac6f98e1155ff61f39
xiaoan <huangxiaoan@yonglibao.com>
2017-11-01 08:39:13 +0800

    add config vendor

--- vendor_install.sh
+++ vendor_install.sh
@@ -3,6 +3,7 @@ current_path=$(cd `dirname $0`; pwd)
 vendor_path=$current_path"/vendor/src"
 
 mysql_path=$vendor_path"/mysql"
+config_path=$vendor_path"/config"
 
 if [ -d "$mysql_path" ]; then
  rm -rf "$mysql_path"
@@ -12,5 +13,16 @@ if [ ! -d "$mysql_path" ]; then
  mkdir "$mysql_path"
 fi
 
+if [ -d "$config_path" ]; then
+ rm -rf "$config_path"
+fi
+
+if [ ! -d "$config_path" ]; then
+ mkdir "$config_path"
+fi
+
 git clone https://github.com/go-sql-driver/mysql.git $mysql_path
 rm -rf $mysql_path"/.git"
+
+git clone https://github.com/larspensjo/config.git $config_path
+rm -rf $config_path"/.git"
