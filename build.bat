4aad3af4e3fafe611c3e0a52f11aea9e9c7a1c53
yuyi <297341015@qq.com>
2017-11-02 09:00:01 +0800

    添加bat自动化脚本

--- build.bat
+++ build.bat
@@ -10,6 +10,9 @@ set vendor_path=%current_path%\vendor
 ::echo %pkg_path%
 ::echo %vendor_path%
 
+::添加环境变量,即在原来的环境变量后加上英文状态下的分号和路径
+set GOPATH=%current_path%\vendor;%current_path%;%PATH%;
+
 ::如果bin目录存在，直接删除掉
 if exist %bin_path% (
  rd %bin_path% /S /Q
@@ -26,3 +29,19 @@ if not exist %vendor_path% (
  md %vendor_path%\src
  call "%current_path%\vendor_install.bat"
 )
+
+cd %~d0
+::进入当前目录
+cd %current_path%
+::build构建项目
+call go build wing-binlog-go
+::install安装
+call go install wing-binlog-go
+::删除根目录下的可执行文件
+del %current_path%\wing-binlog-go
+
+md %bin_path%/config
+
+::拷贝配置文件
+xcopy  %current_path%\src\config\*.* %current_path%\bin\config\ /s /e
+echo "build success"
