702fc6b4725a0eed7f1beb0d054b716844ec0842
xiaoan <huangxiaoan@yonglibao.com>
2017-11-03 07:53:55 +0800

    基础开发

--- test.sh
+++ test.sh
@@ -8,8 +8,27 @@ ini_test_file=$current_path"/src/config/mysql.ini"
 export GOPATH="$current_path/vendor:$current_path"
 echo 123 > /tmp/__test.txt
 cp $ini_test_file "/tmp/__test_mysql.ini"
-cd $library_ip_path && go test
-cd $library_debug_path && go test
-cd $library_path && go test
+
+
+function getDir() {
+    for element in `ls $1`
+    do
+        dir_or_file=$1"/"$element
+        if [ -d $dir_or_file ]
+        then
+            cd $dir_or_file && go test
+            getdir $dir_or_file
+        ##else
+            ##echo $dir_or_file
+        fi
+    done
+}
+
+root_dir="$current_path/src"
+getDir $root_dir
+
+##cd $library_ip_path && go test
+##cd $library_debug_path && go test
+##cd $library_path && go test
 rm /tmp/__test_mysql.ini
 rm /tmp/__test.txt
