70382abbc96eb27d1f063fca1d064d10dcbfee38
xiaoan <huangxiaoan@yonglibao.com>
2017-11-07 07:18:18 +0800

    基础开发

--- vendor_install.sh
+++ vendor_install.sh
@@ -3,7 +3,7 @@ current_path=$(cd `dirname $0`; pwd)
 vendor_path=$current_path"/vendor"
 
 ##添加当前目录和当前目录下的vendor目录到GOPATH环境变量
-export GOPATH="$current_path/vendor:$current_path:$GOPATH"
+export GOPATH="$current_path/vendor:$current_path"
 if [ ! -d "$vendor_path" ]; then
  mkdir "$vendor_path"
  mkdir "$vendor_path/src"
@@ -17,3 +17,19 @@ go get github.com/siddontang/go-mysql/mysql
 go get github.com/BurntSushi/toml
 
 cp -rf $vendor_path/src/* $vendor_path
+
+
+function rmGit()
+{
+    for element in `ls $1`
+    do
+        dir_or_file=$1"/"$element
+        if [ -d $dir_or_file ]
+        then
+            rm -rf $dir_or_file"/.git"
+            rmGit $dir_or_file
+        fi
+    done
+}
+
+rmGit $vendor_path
