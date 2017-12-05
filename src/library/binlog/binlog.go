54983d3454ebe6d0e200f102ae1acfb9a05d7460
yuyi <297341015@qq.com>
2017-12-06 07:42:40 +0800

    兼容unsigned

--- src/library/binlog/binlog.go
+++ src/library/binlog/binlog.go
@@ -115,13 +115,11 @@ func (h *binlogHandler) append(buf *[]byte, edata interface{}, column schema.Tab
 		//if column.IsUnsigned && r < 0 {
 		//	r = 1 << 64 + int64(edata.(int64))
 		//}
-		if column.IsUnsigned {
-			var ur uint64 = 0
-			ur = uint64(edata.(int64))
-			if ur < 0 {
-				ur = 1 << 63 + (1 << 63 + ur)
-			}
-			strconv.AppendUint(*buf, ur, 10)
+		var ur uint64 = 0
+		ur = uint64(edata.(int64))
+		if column.IsUnsigned && ur < 0 {
+			ur = 1 << 63 + (1 << 63 + ur)
+			*buf = strconv.AppendUint(*buf, ur, 10)
 		} else {
 			*buf = strconv.AppendInt(*buf, int64(edata.(int64)), 10)
 		}
