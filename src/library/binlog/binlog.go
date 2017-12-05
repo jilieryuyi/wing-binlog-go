0894a25ecb76b5240b77db4e7bd048340d52c188
yuyi <297341015@qq.com>
2017-12-06 00:17:46 +0800

    兼容unsigned

--- src/library/binlog/binlog.go
+++ src/library/binlog/binlog.go
@@ -119,7 +119,7 @@ func (h *binlogHandler) append(buf *[]byte, edata interface{}, column schema.Tab
 			var ur uint64 = 0
 			ur = uint64(edata.(uint64))
 			if ur < 0 {
-				ur = 1 << 64 + ur
+				ur = 1 << 63 + (1 << 63 + ur)
 			}
 			strconv.AppendUint(*buf, ur, 10)
 		} else {
