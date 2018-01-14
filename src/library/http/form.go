ca3543e88a9ad95095f526aed765cddaeb976245
yuyi <297341015@qq.com>
2018-01-14 10:38:02 +0800

    优化http服务

--- src/library/http/form.go
+++ src/library/http/form.go
@@ -6,98 +6,91 @@ import (
 	"net"
 	"net/http"
 	"time"
-	//log "github.com/sirupsen/logrus"
+	log "github.com/sirupsen/logrus"
 	"errors"
 	"fmt"
 )
+
+type Http struct {
+	url string
+}
+
+func NewHttp(url string) *Http {
+	request := &Http{
+		url : url,
+	}
+	return request
+}
 
 // 超时设置
 var defaultHttpClient = http.Client{
 	Transport: &http.Transport{
 		MaxIdleConnsPerHost: 16,
 		Dial: func(netw, addr string) (net.Conn, error) {
-			//deadline := time.Now().Add(HTTP_POST_TIMEOUT * time.Second)
-
-			//conn, err := net.DialTimeout(netw, addr, time.Second * HTTP_POST_TIMEOUT)
-			//if err != nil {
-			//	return nil, err
-			//}
-
 			dial := net.Dialer{
 				Timeout:   HTTP_POST_TIMEOUT * time.Second,
-				KeepAlive: 86400 * time.Second, //一天之内有效
+				KeepAlive: 86400 * time.Second,
 			}
-
 			conn, err := dial.Dial(netw, addr)
 			if err != nil {
 				return conn, err
 			}
-			//
-			//fmt.Println("connect done, use", conn.LocalAddr().String())
-
-			//conn.SetDeadline(deadline)
 			return conn, nil
 		},
 	},
 }
 
-func Post(addr string, post_data []byte) ([]byte, error) {
-	req, err := http.NewRequest("POST", addr, bytes.NewReader(post_data))
+func Post(addr string, postData []byte) ([]byte, error) {
+	c := NewHttp(addr)
+	return c.Post(postData)
+}
+
+func Get(addr string) ([]byte, error) {
+	c := NewHttp(addr)
+	return c.Get()
+}
+
+func request(method string, url string, data []byte)  ([]byte, error) {
+	reader := bytes.NewReader(data)
+	req, err := http.NewRequest(method, url, reader)
 	if err != nil {
+		log.Errorf("syshttp request error-1:%+v", err)
 		return nil, err
 	}
 	req.Header.Set("Content-Type", "application/json")
 	req.Header.Set("Connection", "keep-alive")
-	// 执行post
 	resp, err := defaultHttpClient.Do(req)
 	if err != nil {
+		log.Errorf("http request error-3:%+v", err)
 		return nil, err
 	}
-	// 关闭io
 	defer resp.Body.Close()
-	// 判断返回状态
 	if resp.StatusCode != http.StatusOK {
-		// 返回异常状态
-		//log.Errorf("http post error, error status back: %d", resp.StatusCode)
-		return nil, errors.New(fmt.Sprintf("错误的状态码：%d", resp.StatusCode)) //ErrorHttpStatus
+		log.Errorf("http request error错误的状态码:%+v", resp.StatusCode)
+		return nil, errors.New(fmt.Sprintf("错误的状态码：%d", resp.StatusCode))
 	}
 	res, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
+		log.Errorf("http request error-4:%+v", err)
 		return nil, err
 	}
 	return res, nil
 }
 
-func Get(addr string) (*[]byte, *int, *http.Header, error) {
-	// 上传JSON数据
-	req, e := http.NewRequest("GET", addr, nil)
-	if e != nil {
-		// 返回异常
-		return nil, nil, nil, e
-	}
-	// 完成后断开连接
-	req.Header.Set("Connection", "keep-alive")
-	// 执行
-	resp, ee := defaultHttpClient.Do(req)
-	if ee != nil {
-		// 返回异常
-		return nil, nil, nil, ee
-	}
-	// 保证I/O正常关闭
-	defer resp.Body.Close()
-	// 判断请求状态
-	if resp.StatusCode == 200 {
-		res, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			// 读取错误,返回异常
-			return nil, nil, nil, err
-		}
-		// 成功，返回数据及状态
-		return &res, &resp.StatusCode, &resp.Header, nil
-	} else {
-		// 失败，返回状态
-		return nil, &resp.StatusCode, nil, nil
-	}
-	// 不会到这里
-	return nil, nil, nil, nil
+func (req *Http) Put(data []byte) ([]byte, error) {
+	return request("PUT", req.url, data)
 }
+
+func (req *Http) Post(data []byte) ([]byte, error) {
+	log.Debugf("post: %s", string(data))
+	return request("POST", req.url, data)
+}
+
+func (req *Http) Get() ([]byte, error) {
+	return request("GET", req.url, nil)
+}
+
+func (req *Http) Delete() ([]byte, error) {
+	return request("DELETE", req.url, nil)
+}
+
--- src/library/services/tcpConfig.go
+++ src/library/services/tcpConfig.go
@@ -1,1 +1,70 @@
 package services
+
+import (
+	"library/file"
+	"github.com/BurntSushi/toml"
+	log "github.com/sirupsen/logrus"
+	"sync"
+	"net"
+	"context"
+)
+
+type tcpClientNode struct {
+	conn             *net.Conn   // 客户端连接进来的资源句柄
+	isConnected      bool        // 是否还连接着 true 表示正常 false表示已断开
+	sendQueue        chan []byte // 发送channel
+	sendFailureTimes int64       // 发送失败次数
+	group            string      // 所属分组
+	recvBuf          []byte      // 读缓冲区
+	recvBytes        int         // 收到的待处理字节数量
+	connectTime      int64       // 连接成功的时间戳
+	sendTimes        int64       // 发送次数，用来计算负载均衡，如果 mode == 2
+}
+
+type tcpGroup struct {
+	name   string           //
+	filter []string         //
+	nodes  []*tcpClientNode //
+}
+
+type TcpService struct {
+	Service
+	Ip               string               // 监听ip
+	Port             int                  // 监听端口
+	recvTimes        int64                // 收到消息的次数
+	sendTimes        int64                // 发送消息的次数
+	sendFailureTimes int64                // 发送失败的次数
+	lock             *sync.Mutex          // 互斥锁，修改资源时锁定
+	groups           map[string]*tcpGroup //
+	enable           bool                 //
+	ctx              *context.Context     //
+	listener         *net.Listener        //
+	wg               *sync.WaitGroup      //
+}
+
+type tcpGroupConfig struct { // group node in toml
+	Name   string   // = "group1"
+	Filter []string //
+}
+
+type TcpConfig struct {
+	Listen string `toml:"listen"`
+	Port   int    `toml:"port"`
+	Enable bool   `toml:"enable"`
+	Groups map[string]tcpGroupConfig
+}
+
+func getTcpConfig() (*TcpConfig, error) {
+	var tcp_config TcpConfig
+	tcp_config_file := file.GetCurrentPath() + "/config/tcp.toml"
+	wfile := file.WFile{tcp_config_file}
+	if !wfile.Exists() {
+		log.Warnf("配置文件%s不存在 %s", tcp_config_file)
+		return nil, ErrorFileNotFound
+	}
+	if _, err := toml.DecodeFile(tcp_config_file, &tcp_config); err != nil {
+		log.Println(err)
+		return nil, ErrorFileParse
+	}
+	return &tcp_config, nil
+}
