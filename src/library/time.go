db25625a5ebd212e871bfe7c63f502703a2d362d
yuyi <297341015@qq.com>
2018-01-15 14:19:13 +0800

    add time format helper

--- src/library/time.go
+++ src/library/time.go
@@ -2,6 +2,7 @@ package library
 
 import (
 	"time"
+	"github.com/hashicorp/consul/command/info"
 )
 
 func GetDayTime() string {
@@ -14,4 +15,19 @@ func GetDay(format string) string {
 
 func GetTimeStamp() int64 {
 	return time.Now().Unix()
+}
+
+// 时间戳格式化为 Y-m-d H:i:s 格式
+func Format(ts int64) string {
+	return time.Unix(ts, 0).Format("2006-01-02 15:04:05")
+}
+
+// 将字符串转换为时间戳
+// 比如 2018-01-01 08:00:00 转换为时间戳
+func ToTimestamp(t string) (int64, error) {
+	u, err := time.Parse("2006-01-02 15:04:05", t)
+	if err != nil {
+		return 0, err
+	}
+	return u.Unix(), nil
 }
