1a6c8f6c406d3c85841e8d08eda547b9f2597ad9
yuyi <297341015@qq.com>
2018-01-17 19:55:46 +0800

    remove cluster code

--- src/library/binlog/handler_test.go
+++ src/library/binlog/handler_test.go
@@ -3,25 +3,12 @@ package binlog
 import (
 	"testing"
 	"os"
-	log "github.com/sirupsen/logrus"
 )
-
-func init() {
-	log.SetFormatter(&log.TextFormatter{
-		TimestampFormat: "2006-01-02 15:04:05",
-		ForceColors:      true,
-		QuoteEmptyFields: true,
-		FullTimestamp:    true,
-	})
-	log.SetLevel(5)
-}
-
 
 func TestBinlogHandler_SaveBinlogPostionCache(t *testing.T) {
 	binfile := "mysql-bin.000059"
 	pos := int64(123456)
 	eventIndex := int64(20)
-
 	h := &binlogHandler{}
 	var err error
 	flag := os.O_RDWR | os.O_CREATE | os.O_SYNC // | os.O_TRUNC
@@ -30,20 +17,13 @@ func TestBinlogHandler_SaveBinlogPostionCache(t *testing.T) {
 		t.Errorf("binlog open cache file error: %+v", err)
 	}
 	h.SaveBinlogPostionCache(binfile, pos, eventIndex)
-
 	s, p, e := h.getBinlogPositionCache()
-	log.Debugf("%s, %d, %d", s, p, e)
-
-	log.Debugf("%+v, %+v", []byte(s), []byte(binfile))
-
 	if s != binfile {
 		t.Errorf("getBinlogPositionCache binfile error")
 	}
-
 	if pos != p {
 		t.Errorf("getBinlogPositionCache pos error")
 	}
-
 	if e != eventIndex {
 		t.Errorf("getBinlogPositionCache eventIndex error")
 	}
