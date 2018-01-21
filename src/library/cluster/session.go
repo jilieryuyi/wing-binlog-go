668561e3c9fa689d729df4c6788af26a5c6f1f20
yuyi <297341015@qq.com>
2018-01-21 22:14:23 +0800

    ha support

--- src/library/cluster/session.go
+++ src/library/cluster/session.go
@@ -6,10 +6,24 @@ import (
 	"fmt"
 	"time"
 	"math/rand"
+	log "github.com/sirupsen/logrus"
 )
+
+func GetRandomString(l int) string{
+	str := "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	bytes := []byte(str)
+	bl := len(bytes)
+	result := []byte{}
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	for i := 0; i < l; i++ {
+		result = append(result, bytes[r.Intn(bl)])
+	}
+	return string(result)
+}
 
 func GetSession() string {
 	sessionFile := path.CurrentPath + "/cache/session"
+	log.Debugf("session file: %s", sessionFile)
 	if file.Exists(sessionFile) {
 		data := file.Read(sessionFile)
 		if data != "" {
@@ -17,7 +31,8 @@ func GetSession() string {
 		}
 	}
 	//write a new session
-	session := fmt.Sprintf("%d-%d-%d-%d", time.Now().Unix(), rand.Intn(9999), rand.Intn(9999), rand.Intn(9999))
+	session := fmt.Sprintf("%d-%s-%s-%s", time.Now().Unix(),
+		GetRandomString(4), GetRandomString(4), GetRandomString(4))
 	dir := path.GetParent(sessionFile)
 	path.Mkdir(dir)
 	n := file.Write(sessionFile, session, false)
