7dcd6b7069bedf99425a6f520694b58859f074b2
yuyi <297341015@qq.com>
2018-02-08 23:15:30 +0800

    code optimize

--- src/library/unix/cmd.go
+++ src/library/unix/cmd.go
@@ -1,35 +1,34 @@
-package command
+package unix
 
 import (
 	"fmt"
-	"library/unix"
 	log "github.com/sirupsen/logrus"
 )
 
 func Stop() {
-	client := unix.NewUnixClient()
-	msg := client.Pack(unix.CMD_STOP, "")
+	client := NewUnixClient()
+	msg := client.Pack(CMD_STOP, "")
 	client.Send(msg)
 	client.Close()
 }
 
 func Reload(cmd string) {
-	client := unix.NewUnixClient()
-	msg := client.Pack(unix.CMD_RELOAD, cmd)
+	client := NewUnixClient()
+	msg := client.Pack(CMD_RELOAD, cmd)
 	client.Send(msg)
 	client.Close()
 }
 
 func JoinTo(dns string) {
-	client := unix.NewUnixClient()
-	msg := client.Pack(unix.CMD_JOINTO, dns)
+	client := NewUnixClient()
+	msg := client.Pack(CMD_JOINTO, dns)
 	client.Send(msg)
 	client.Close()
 }
 
 func ShowMembers() {
-	client := unix.NewUnixClient()
-	msg := client.Pack(unix.CMD_SHOW_MEMBERS, "")
+	client := NewUnixClient()
+	msg := client.Pack(CMD_SHOW_MEMBERS, "")
 	client.Send(msg)
 	buf, err := client.Read()
 	if err != nil {
@@ -40,8 +39,8 @@ func ShowMembers() {
 }
 
 func Clear() {
-	client := unix.NewUnixClient()
-	msg := client.Pack(unix.CMD_CLEAR, "")
+	client := NewUnixClient()
+	msg := client.Pack(CMD_CLEAR, "")
 	client.Send(msg)
 	_, err := client.Read()
 	if err != nil {
--- src/wing-binlog-go/wing-binlog-go.go
+++ src/wing-binlog-go/wing-binlog-go.go
@@ -3,25 +3,17 @@ package main
 import (
 	"flag"
 	"fmt"
-	"io/ioutil"
-	"net/http"
-	_ "net/http/pprof"
 	"os"
 	"os/signal"
-	"runtime"
 	"syscall"
-	"time"
 	"library/app"
 	"library/binlog"
-	"library/command"
-	"library/file"
 	"library/services"
 	"library/unix"
 	_ "github.com/go-sql-driver/mysql"
 	log "github.com/sirupsen/logrus"
-	mlog "library/log"
-	"library/path"
 	"github.com/sevlyar/go-daemon"
+	"library/path"
 )
 
 var (
@@ -42,133 +34,49 @@ var (
 	d              = flag.Bool("d", false, "-deamon or -d, run as deamon process")
 )
 
-const (
-	VERSION = "1.0.0"
-)
-
-var (
-	pid = path.CurrentPath + "/wing-binlog-go.pid"
- 	appConfig, _ = app.GetAppConfig()
-)
-
-// write pid file
-func writePid() {
-	data := []byte(fmt.Sprintf("%d", os.Getpid()))
-	ioutil.WriteFile(pid, data, 0777)
-}
-
-// delete pid file
-func clearPid() {
-	file.Delete(pid)
-}
-
-// kill process by pid file
-//func killPid() {
-//	dat, _ := ioutil.ReadFile(pid)
-//	fmt.Print(string(dat))
-//	pid, _ := strconv.Atoi(string(dat))
-//	log.Debugf("try to kill process: %d", pid)
-//	//err := syscall.Kill(pid, syscall.SIGTERM)
-//	//log.Println(err)
-//}
-
-// pprof tool support
-func pprofService() {
-	go func() {
-		//http://localhost:6060/debug/pprof/  内存性能分析工具
-		//go tool pprof logDemo.exe --text a.prof
-		//go tool pprof your-executable-name profile-filename
-		//go tool pprof your-executable-name http://localhost:6060/debug/pprof/heap
-		//go tool pprof wing-binlog-go http://localhost:6060/debug/pprof/heap
-		//https://lrita.github.io/2017/05/26/golang-memory-pprof/
-		//然后执行 text
-		//go tool pprof -alloc_space http://127.0.0.1:6060/debug/pprof/heap
-		//top20 -cum
-
-		//下载文件 http://localhost:6060/debug/pprof/profile
-		//分析 go tool pprof -web /Users/yuyi/Downloads/profile
-		if appConfig.PprofListen != "" {
-			http.ListenAndServe(appConfig.PprofListen, nil)
-		}
-	}()
-}
-
-func usage() {
-	fmt.Println("*********************************************************************")
-	fmt.Println("wing-binlog-go                                   --start service")
-	fmt.Println("wing-binlog-go -version                          --show version info")
-	fmt.Println("wing-binlog-go -stop                             --stop service")
-	fmt.Println("wing-binlog-go -service-reload  http             --reload http service")
-	fmt.Println("wing-binlog-go -service-reload  tcp              --reload tcp service")
-	fmt.Println("wing-binlog-go -service-reload  websocket        --reload websocket service")
-	fmt.Println("wing-binlog-go -service-reload  all              --reload all service")
-	fmt.Println("wing-binlog-go -members                          --show cluster members")
-	fmt.Println("wing-binlog-go -d|-daemon                        --run as daemon process")
-	fmt.Println("*********************************************************************")
-}
-
-func init() {
-	time.LoadLocation(appConfig.TimeZone)
-	log.SetFormatter(&log.TextFormatter{
-		TimestampFormat: "2006-01-02 15:04:05",
-		ForceColors:      true,
-		QuoteEmptyFields: true,
-		FullTimestamp:    true,
-	})
-	log.AddHook(mlog.ContextHook{})
-	log.SetLevel(log.Level(appConfig.LogLevel)) //log.DebugLevel)
-	//log.Debugf("wing-binlog-go基础配置：%+v\n", app_config)
-	//log.ResetOutHandler()
-	//u := data.User{"admin", "admin"}
-	//log.Println("用户查询：",u.Get())
-	//u = data.User{"admin", "admin1"}
-	//log.Println("用户查询：",u.Get())
-	writePid()
-	//标准输出重定向
-	//library.Reset()
-}
-
-func commandService() bool {
+func Cmd() bool {
 	// 显示版本信息
 	if *version {
-		fmt.Println(VERSION)
+		fmt.Println(app.VERSION)
 		return true
 	}
 	// 停止服务
 	if *stop {
-		command.Stop()
+		unix.Stop()
 		return true
 	}
 	// 重新加载服务
 	if *serviceReload != "" {
-		command.Reload(*serviceReload)
+		unix.Reload(*serviceReload)
 		return true
 	}
 	// 帮助
 	if *help {
-		usage()
+		app.Usage()
 		return true
 	}
 	if *members {
-		command.ShowMembers()
+		unix.ShowMembers()
 		return true
 	}
 	return false
 }
 
+
 func main() {
+	flag.Parse()
 	defer func() {
 		if err := recover(); err != nil {
 			log.Errorf("%+v", err)
 		}
 	}()
-	flag.Parse()
-	if commandService() {
+	// if use cmd params
+	if Cmd() {
 		return
 	}
 	if *deamon || *d {
 		cntxt := &daemon.Context{
-			PidFileName: pid,
+			PidFileName: app.Pid,
 			PidFilePerm: 0644,
 			LogFileName: path.CurrentPath + "/logs/wing-binlog-go.log",
 			LogFilePerm: 0640,
@@ -185,46 +93,45 @@ func main() {
 		}
 		defer cntxt.Release()
 	}
-
+	// app init
 	app.DEBUG = *debug
-	// 退出程序时删除pid文件
-	defer clearPid()
-	// 性能测试
-	pprofService()
-	cpu := runtime.NumCPU()
-	runtime.GOMAXPROCS(cpu) //指定cpu为多核运行 旧版本兼容
+	app.Init()
+	// clear some resource after exit
+	defer app.Release()
 
-	appContext := app.NewContext()
-	appContext.PidFile = pid
-
-	// 各种通信服务
+	appContext  := app.NewContext()
 	httpService := services.NewHttpService(appContext)
 	tcpService  := services.NewTcpService(appContext)
 
-	// 核心binlog服务
 	blog := binlog.NewBinlog(appContext)
-	// 注册tcp、http、websocket服务
 	blog.RegisterService("tcp", tcpService)
 	blog.RegisterService("http", httpService)
-	// here can be any drive interface form library.Cluster
-	//blog.RegisterDrive(clu)
 	blog.Start()
 
-	// unix socket服务，用户本地指令控制
+	// unix socket use for cmd support
 	server := unix.NewUnixServer(appContext, blog)
 	server.Start()
 	defer server.Close()
 
-	sc := make(chan os.Signal, 1)
-	signal.Notify(sc,
-		os.Kill,
-		os.Interrupt,
-		syscall.SIGHUP,
-		syscall.SIGINT,
-		syscall.SIGTERM,
-		syscall.SIGQUIT)
-	<-sc
-	// 优雅的退出程序
+	go func() {
+		// wait for exit signal
+		sc := make(chan os.Signal, 1)
+		signal.Notify(sc,
+			os.Kill,
+			os.Interrupt,
+			syscall.SIGHUP,
+			syscall.SIGINT,
+			syscall.SIGTERM,
+			syscall.SIGQUIT)
+		<-sc
+		appContext.CancelChan <- struct{}{}
+	}()
+
+	// wait exit
+	select {
+		case <- appContext.CancelChan:
+	}
+
 	appContext.Cancel()
 	blog.Close()
 	fmt.Println("wing binlog service exit...")
