90868b8c5b6bc351bba2bb8fd1d0f55caf4b4479
yuyi <297341015@qq.com>
2018-02-09 13:25:28 +0800

    solving tcp service and tcp agent cyclic dependence

--- src/library/services/service_util.go
+++ src/library/services/service_util.go
@@ -40,3 +40,19 @@ func GetTcpConfig() (*TcpConfig, error) {
 	}
 	return &tcpConfig, nil
 }
+
+
+func pack(cmd int, msg string) []byte {
+	m  := []byte(msg)
+	l  := len(m)
+	r  := make([]byte, l+6)
+	cl := l + 2
+	r[0] = byte(cl)
+	r[1] = byte(cl >> 8)
+	r[2] = byte(cl >> 16)
+	r[3] = byte(cl >> 24)
+	r[4] = byte(cmd)
+	r[5] = byte(cmd >> 8)
+	copy(r[6:], m)
+	return r
+}
