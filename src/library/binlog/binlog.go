3d0bce161c3cdd8f025e5787b03c668887bb7355
yuyi <297341015@qq.com>
2018-02-10 21:05:06 +0800

    add check tcp alive

--- src/library/binlog/binlog.go
+++ src/library/binlog/binlog.go
@@ -43,9 +43,9 @@ func (h *Binlog) Close() {
 	}
 	log.Warn("binlog service exit")
 	h.StopService(true)
-	h.cacheHandler.Close()
 	close(h.startServiceChan)
 	close(h.stopServiceChan)
+	h.cacheHandler.Close()
 	for name, service := range h.services {
 		log.Debugf("%s service exit", name)
 		service.Close()
@@ -103,7 +103,6 @@ func (h *Binlog) lookService() {
 			}
 		}
 	}()
-
 	go func(){
 		for {
 			select {
@@ -131,60 +130,10 @@ func (h *Binlog) lookService() {
 
 func (h *Binlog) StopService(exit bool) {
 	h.stopServiceChan <- exit
-	//log.Debug("binlog service stop")
-	//h.lock.Lock()
-	//defer h.lock.Unlock()
-	//isRunning := atomic.LoadInt32(&h.isRunning)
-	//if isRunning > 0 && !exit {
-	//	h.handler.Close()
-	//	//reset handler
-	//	h.setHandler()
-	//} else {
-	//	h.SaveBinlogPostionCache(h.lastBinFile,
-	//		int64(h.lastPos),
-	//		atomic.LoadInt64(&h.EventIndex))
-	//}
-	//atomic.StoreInt32(&h.isRunning, 0)
 }
 
 func (h *Binlog) StartService() {
 	h.startServiceChan <- struct{}{}
-	//log.Debug("binlog service start")
-	//h.lock.Lock()
-	//defer h.lock.Unlock()
-	//isRunning := atomic.LoadInt32(&h.isRunning)
-	//if isRunning > 0 {
-	//	log.Debug("binlog service is still running")
-	//	return
-	//}
-	//atomic.StoreInt32(&h.isRunning, 1)
-	//go func() {
-	//	for {
-	//		if h.lastBinFile == "" {
-	//			log.Warn("binlog lastBinFile is empty, wait for init")
-	//			time.Sleep(time.Second)
-	//			continue
-	//		}
-	//		break
-	//	}
-	//	startPos := mysql.Position{
-	//		Name: h.lastBinFile,
-	//		Pos:  h.lastPos,
-	//	}
-	//	for {
-	//		if h.handler == nil {
-	//			log.Warn("binlog handler is nil, wait for init")
-	//			time.Sleep(time.Second)
-	//			continue
-	//		}
-	//		break
-	//	}
-	//	err := h.handler.RunFrom(startPos)
-	//	if err != nil {
-	//		log.Warnf("binlog service exit with error: %+v", err)
-	//		return
-	//	}
-	//}()
 }
 
 func (h *Binlog) Start() {
@@ -197,7 +146,7 @@ func (h *Binlog) Start() {
 		h.StartService()
 	} else {
 		go func() {
-			var serviceIp= ""
+			var serviceIp = ""
 			var port= 0
 			for {
 				serviceIp, port = h.GetLeader()
