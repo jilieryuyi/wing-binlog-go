c69e98892a223d580211d208248a3c5fefc4b51f
yuyi <297341015@qq.com>
2018-03-01 19:18:06 +0800

    optimize client.go

--- clients/client.go
+++ clients/client.go
@@ -21,44 +21,69 @@ const (
 	CMD_STOP
 	CMD_RELOAD
 	CMD_SHOW_MEMBERS
+	CMD_POS
 )
+
+const (
+	clientOffline = 1 << iota
+	clientOnline
+)
+
+const (
+	nodeOffline = 1 << iota
+	nodeOnline
+)
+
 const (
 	tcpDefaultReadBufferSize = 4096
 )
 
+func hasCmd(cmd int) bool {
+	return cmd == CMD_SET_PRO ||
+		cmd == CMD_AUTH ||
+		cmd == CMD_ERROR||
+		cmd == CMD_TICK ||
+		cmd == CMD_EVENT||
+		cmd == CMD_AGENT||
+		cmd == CMD_STOP||
+		cmd == CMD_RELOAD||
+		cmd == CMD_SHOW_MEMBERS||
+		cmd == CMD_POS
+}
+
 type Client struct {
 	node *Node
-	isClose bool
 	lock *sync.Mutex
 	buffer []byte
-	starttime int64
+	startTime int64
 	Services []*service
 	times int64
+	status int
 }
 
 type Node struct {
 	conn *net.TCPConn
-	isConnect bool
+	status int
 }
 
 func NewClient(s []*service) *Client{
 	client := &Client{
-		isClose   : true,
+		status    : clientOffline,
 		node      : nil,
 		lock      : new(sync.Mutex),
 		buffer    : make([]byte, 0),
-		starttime : time.Now().Unix(),
+		startTime : time.Now().Unix(),
 		Services  : s,
 		times     : 0,
 	}
 	return client
 }
 
-func (client *Client) init(ip string, port int) {
-	log.Debugf("init tcp connect, connect to %s:%d", ip, port)
+func (client *Client) connect(ip string, port int) {
+	log.Debugf("connect to %s:%d", ip, port)
 	client.lock.Lock()
 	defer client.lock.Unlock()
-	if client.node != nil && client.node.isConnect {
+	if client.node != nil && client.node.status & nodeOnline > 0 {
 		client.disconnect()
 	}
 	tcpAddr, err := net.ResolveTCPAddr("tcp4", fmt.Sprintf("%s:%d", ip, port))
@@ -68,15 +93,19 @@ func (client *Client) init(ip string, port int) {
 	}
 	conn, err := net.DialTCP("tcp", nil, tcpAddr)
 	client.node = &Node{
-		conn:conn,
-		isConnect:true,
+		conn : conn,
+		status : nodeOnline,
 	}
 	if err != nil {
 		log.Errorf("start client with error: %+v", err)
-		client.node.isConnect = false
+		client.node.status ^= nodeOnline
+		client.node.status |= nodeOffline
 		client.node.conn = nil
 	} else {
-		client.isClose = false
+		if client.status & clientOffline > 0 {
+			client.status ^= clientOffline
+			client.status |= clientOnline
+		}
 	}
 }
 
@@ -97,6 +126,7 @@ func (client *Client) pack(cmd int, content string) []byte {
 	r = append(r[:6], c...)
 	return r
 }
+
 func (client *Client) setPro(content string) []byte {
 	// 数据打包
 	c := []byte(content)
@@ -116,9 +146,9 @@ func (client *Client) setPro(content string) []byte {
 	return r
 }
 
-
-func (client *Client) Start() {
-	keepalive := client.pack(CMD_TICK, "")
+func (client *Client) keepalive() {
+	data := client.pack(CMD_TICK, "")
+	dl := len(data)
 	go func() {
 		for {
 			if client.node == nil {
@@ -126,56 +156,49 @@ func (client *Client) Start() {
 				continue
 			}
 			client.lock.Lock()
-			if client.node.conn != nil {
-				client.node.conn.Write(keepalive)
+			if client.node.conn != nil && client.node.status & nodeOnline > 0 {
+				n, err := client.node.conn.Write(data)
+				if err != nil {
+					client.disconnect()
+				}
+				if n != dl {
+					log.Errorf("发送数据不完整")
+				}
 			}
 			client.lock.Unlock()
 			time.Sleep(time.Second * 5)
 		}
 	}()
+}
+
+func (client *Client) Start() {
+	client.keepalive()
+	var readBuffer [tcpDefaultReadBufferSize]byte
 	for {
 		for _, server := range client.Services {
-			client.init(server.ip, server.port)
-			if  client.node == nil {
+			client.connect(server.ip, server.port)
+			if  client.node == nil || client.node.conn == nil || client.node.status & nodeOffline > 0 {
 				time.Sleep(time.Second)
-				log.Warnf("node is nil")
-				continue
-			}
-			if client.node.conn == nil {
-				time.Sleep(time.Second)
-				log.Warnf("conn is nil")
-				continue
-			}
-			if !client.node.isConnect {
-				time.Sleep(time.Second)
-				log.Warnf("isConnect is false")
 				continue
 			}
 			log.Debugf("====================client start====================")
-			//握手包
-			clientH := client.setPro(server.groupName)
-			var readBuffer [tcpDefaultReadBufferSize]byte
-			if client.isClose {
+			if client.status & clientOffline > 0 {
 				return
 			}
-			//握手
+			//加入到分组
+			clientH := client.setPro(server.groupName)
 			client.node.conn.Write(clientH)
 			for {
-				if client.isClose {
+				if client.status & clientOffline > 0 {
 					return
 				}
-				buf := readBuffer[:tcpDefaultReadBufferSize]
-				//清空旧数据 memset
-				for i := range buf {
-					buf[i] = byte(0)
-				}
-				size, err := client.node.conn.Read(buf[0:])
+				size, err := client.node.conn.Read(readBuffer[0:])
 				if err != nil || size <= 0 {
 					log.Warnf("client read with error: %+v", err)
 					client.disconnect()
 					break
 				}
-				client.onMessage(buf[:size])
+				client.onMessage(readBuffer[:size])
 			}
 		}
 	}
@@ -184,30 +207,34 @@ func (client *Client) Start() {
 func (client *Client) disconnect() {
 	client.lock.Lock()
 	defer client.lock.Unlock()
-	if client.node == nil || !client.node.isConnect {
+	if client.node == nil || client.node.status & nodeOffline > 0 {
 		return
 	}
 	log.Warnf("---------------client disconnect---------------")
 	client.node.conn.Close()
-	client.node.isConnect = false
+	if client.node.status & nodeOnline > 0 {
+		client.node.status ^= nodeOnline
+		client.node.status |= nodeOffline
+	}
 }
 
 func (client *Client) Close() {
-	if client.isClose {
+	if client.status & clientOffline > 0 {
 		log.Debugf("client close was called, but not running")
 		return
 	}
 	log.Warnf("---------------client close---------------")
 	client.disconnect()
 	client.lock.Lock()
-	client.isClose = true
+	if client.status & clientOnline > 0 {
+		client.status ^= clientOnline
+		client.status |= clientOffline
+	}
 	client.lock.Unlock()
 }
 
 func (client *Client) onMessage(msg []byte) {
 	client.buffer = append(client.buffer, msg...)
-	//todo send broadcast
-	//这里还需要解包数据
 	for {
 		bufferLen := len(client.buffer)
 		if bufferLen < 6 {
@@ -217,17 +244,22 @@ func (client *Client) onMessage(msg []byte) {
 		contentLen := int(client.buffer[0]) | int(client.buffer[1]) << 8 | int(client.buffer[2]) << 16 | int(client.buffer[3]) << 24
 		//2字节 command
 		cmd := int(client.buffer[4]) | int(client.buffer[5]) << 8
+		if !hasCmd(cmd) {
+			log.Errorf("cmd=%d 不支持的cmd事件", cmd)
+			client.buffer = make([]byte, 0)
+			return
+		}
 		//数据未接收完整，等待下一次处理
 		if bufferLen < 4 + contentLen {
 			return
 		}
 		dataB := client.buffer[6:4 + contentLen]
-		switch(cmd) {
+		switch cmd {
 		case CMD_EVENT:
 			client.times++
 			log.Debugf("收到%d次数据库事件", client.times)
 			p := int64(0)
-			sp := time.Now().Unix() - client.starttime
+			sp := time.Now().Unix() - client.startTime
 			if sp > 0 {
 				p = int64(client.times/sp)
 			}
@@ -235,10 +267,21 @@ func (client *Client) onMessage(msg []byte) {
 			var data interface{}
 			json.Unmarshal(dataB, &data)
 			log.Debugf("%+v", data)
+		case CMD_SET_PRO:
+		case CMD_AUTH:          // 认证（暂未使用）
+		case CMD_ERROR:         // 错误响应
+		case CMD_TICK:        // 心跳包
+		case CMD_AGENT:
+		case CMD_STOP:
+		case CMD_RELOAD:
+		case CMD_SHOW_MEMBERS:
+		case CMD_POS:
 		default:
-			//log.Debugf("收到其他消息")
+			log.Errorf("cmd=%d 不支持的cmd事件", cmd)
+			client.buffer = make([]byte, 0)
+			return
 		}
-		//数据移动，清除已读数据
+		//清除已读数据
 		client.buffer = append(client.buffer[:0], client.buffer[contentLen + 4:]...)
 	}
 }
@@ -251,6 +294,7 @@ type service struct {
 }
 
 func main() {
+	//初始化debug终端输出日志支持
 	log.SetFormatter(&log.TextFormatter{
 		TimestampFormat: "2006-01-02 15:04:05",
 		ForceColors:      true,
@@ -263,7 +307,7 @@ func main() {
 
 	if len(os.Args) >= 3 {
 		defaultIp = os.Args[1]
-		port, _:= strconv.Atoi(os.Args[2])//strconv.ParseInt(os.Args[1], 10, 64)
+		port, _:= strconv.Atoi(os.Args[2])
 		defaultPort = port
 	}
 
@@ -272,27 +316,9 @@ func main() {
 		ip : defaultIp,
 		port : defaultPort,
 	}
-	//ser2 := &service{
-	//	groupName : "group1",
-	//	ip : "127.0.0.1",
-	//	port :9998,
-	//}
-	//ser3 := &service{
-	//	groupName : "group1",
-	//	ip : "127.0.0.1",
-	//	port :10010,
-	//}
-	//ser4 := &service{
-	//	groupName : "group1",
-	//	ip : "127.0.0.1",
-	//	port :10009,
-	//}
 
 	s := make([]*service, 0)
 	s = append(s, ser1)
-	//s = append(s, ser2)
-	//s = append(s, ser3)
-	//s = append(s, ser4)
 
 	client := NewClient(s)
 	client.Start()
