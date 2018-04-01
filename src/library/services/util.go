1c58784f9ce70fc48c58a8752a00a775f0ac8198
yuyi <297341015@qq.com>
2018-04-01 22:33:28 +0800

    move http and tcp service to service_plugin dir

--- src/library/services/util.go
+++ src/library/services/util.go
@@ -14,3 +14,19 @@ func MatchFilters(filters []string, table string) bool {
 	}
 	return false
 }
+
+
+func Pack(cmd int, msg []byte) []byte {
+	//m  := []byte(msg)
+	l  := len(msg)
+	r  := make([]byte, l+6)
+	cl := l + 2
+	r[0] = byte(cl)
+	r[1] = byte(cl >> 8)
+	r[2] = byte(cl >> 16)
+	r[3] = byte(cl >> 24)
+	r[4] = byte(cmd)
+	r[5] = byte(cmd >> 8)
+	copy(r[6:], msg)
+	return r
+}
